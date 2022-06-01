8a28e83f2d48ba0d889766d33f32c4f0ccbbafbf
JLMackin <Jonnnmackin@gmail.com>
2022-05-31 22:18:04 -0700

    first

---
+++ src/main/java/com/JLMthingsNstuff/JLMSite/JlmSiteApplication.java
@@ -0,0 +1,13 @@
+package com.JLMthingsNstuff.JLMSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlmSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JlmSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JLMthingsNstuff/JLMSite/JlmSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JLMthingsNstuff.JLMSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlmSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
