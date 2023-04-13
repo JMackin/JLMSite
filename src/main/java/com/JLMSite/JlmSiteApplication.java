d0096f15aeca83638cfe4170e1e917e0bc0dc65d
John Mackin <johnluthermackin@jlmw.xyz>
2023-04-13 12:46:37 -0500

    PGP key page and clock attributes

--- src/main/java/com/JLMSite/JlmSiteApplication.java
+++ src/main/java/com/JLMSite/JlmSiteApplication.java
@@ -1,12 +1,21 @@
 package com.JLMSite;
 
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import jakarta.persistence.EntityManagerFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
 
 @EnableJpaRepositories
 @EntityScan
@@ -17,5 +26,8 @@ public class JlmSiteApplication {
 		ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(JlmSiteApplication.class, args);
 
 	}
+
+
+
 
 }
---
+++ src/main/java/com/JLMSite/controller/SignatureController.java
@@ -0,0 +1,14 @@
+package com.JLMSite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SignatureController {
+    @GetMapping("/PGP_Signature")
+    public String resume()
+    {
+        return "JLMPgp.html";
+    }
+
+}
